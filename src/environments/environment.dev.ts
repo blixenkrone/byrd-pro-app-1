c103a62f8745d82ba0e646c79770c641698ba3f2
blixenkrone <sblixenkrone@gmail.com>
2020-04-06 11:42:46 +0200

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -17,4 +17,4 @@ export const environment = {
 /*
  * For easier debugging in development mode, you can import the following file
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
